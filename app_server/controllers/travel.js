d447994d047df8d643db1eb084ff5ad580a790fe
neelup84 <https://github.com/neelup84>
2022-07-31 23:40:33 -0500

    api-part2

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,8 +1,59 @@
-const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', {title: pageTitle});
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
+
+/* render travel list view */
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
+
+    // if the result was not an array, make it an array and report error
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    }
+    else {
+        if (!responseBody.length) {
+            message = 'No trips exist in database';
+        }
+    }
+
+    // render the page
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody,
+        message
+    });
 };
 
+
+/* GET travel list view */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    // construct the request
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    // send the request to the api and get a response. Use the response to render the travel page
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            console.log('statusCode: ', response && response.statusCode)
+            renderTravelList(req, res, body);
+        }
+    )
+};
+
+
 module.exports = {
-    travel
+    travelList
 }
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const controller = require('../controllers/travel');
 
 /* GET home page. */
-router.get('/', controller.travel);
+router.get('/', controller.travelList);
 
 module.exports = router;
 
