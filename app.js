8d940a1bdc9ed6627254dada46f6ea21a690524d
neelup84 <https://github.com/neelup84>
2022-08-08 00:24:18 -0500

    Angular SPA application

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// include reference to handlebars code
 const hbs = require('hbs');
+// set up database connection
 require('./app_api/models/db');
 
-var indexRouter = require('./app_server/routes/index');
+// set up routers
+var indexRouter = require('./app_server/routes/index_router');
 var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel');
+var travelRouter = require('./app_server/routes/travel_router');
+var aboutRouter = require('./app_server/routes/about_router');
+var contactRouter = require('./app_server/routes/contact_router');
+var mealsRouter = require('./app_server/routes/meals_router');
+var newsRouter = require('./app_server/routes/news_router');
+var roomsRouter = require('./app_server/routes/rooms_router');
 const apiRouter = require('./app_api/routes/index');
 
 var app = express();
@@ -17,10 +25,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
-//register handlebar partials (https://www.npmjs.com/package/hbs)
-  hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
-
-app.set('view engine', 'hbs');
+// register handlebars partials (https://www.npmjs.com/package/hbs)
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,9 +34,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// allow CORS
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requesteed-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+})
+
+// use routers for specific URLs
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
+app.use('/meals', mealsRouter);
+app.use('/news', newsRouter);
+app.use('/rooms', roomsRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
