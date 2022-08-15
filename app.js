9e62a010057f250dcbd3a334d1fdee3f8c78bdbc
neelup84 <https://github.com/neelup84>
2022-08-14 20:28:45 -0500

    Backend security

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -33,11 +35,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.intialize());
 
 // allow CORS
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requesteed-With, Content-Type, Accept');
+res.header('Access-Control-Allow-Headers', 'Origin, X-Requesteed-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 })
--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,48 +1,37 @@
 const mongoose = require('mongoose');
-const host = process.env.DB_HOST || '127.0.0.1';
-const dbURI = 'mongodb://${host}/travlr';
-const readline = require('readline');
+const host = process.env.DB_HOST || '127.0.0.1'
+const dbURL = `mongodb://${host}/travlr`;
+const readLine = require('readline');
 
-//*mongoose.connect(dbURI);
-
-//mongoose.connection.on('connected', () => {
-//  console.log(`Mongoose connected to ${dbURI}`);
-//});
-//mongoose.connection.on('error', err => {
-  //console.log('Mongoose connection error:', err);
-//});
-//mongoose.connection.on('disconnected', () => {
- // console.log('Mongoose disconnected');
-//}); 
-mongoose.set('useUnifieldToplolgy', true);
-
-const connect = () => {
-    setTimeout(() => mongoose.connect(dbURI,{
-        useNewUrlParser: true,
-        useCreateIndex: true
-    }), 1000);
+if (process.env.NODE_ENV === 'production') {
+  dbURL = process.env.DB_HOST || process.env.MONGODB_URI;
 }
 
+const connect = () => {
+  setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }), 1000);
+}
+
+mongoose.connection.on('connected', () => {
+  console.log('connected');
+});
+
 mongoose.connection.on('error', err => {
-    console.log('error: ' + err)
-    return connect();
+  console.log('error: ' + err);
+  return connect();
 });
 
 mongoose.connection.on('disconnected', () => {
-    console.log('disconnected');
-
+  console.log('disconnected');
 });
 
-if (process.platform === 'win32'){
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stout
-    });
-
-    rl.on('SIGINT', () => {
-        process.emit("SIGINT");
-    });
-    
+if (process.platform === 'win32') {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on ('SIGINT', () => {
+    process.emit("SIGINT");
+  });
 }
 
 const gracefulShutdown = (msg, callback) => {
@@ -52,19 +41,16 @@ const gracefulShutdown = (msg, callback) => {
   });
 };
 
-// For nodemon restarts                                 
 process.once('SIGUSR2', () => {
   gracefulShutdown('nodemon restart', () => {
     process.kill(process.pid, 'SIGUSR2');
   });
 });
-// For app termination
 process.on('SIGINT', () => {
   gracefulShutdown('app termination', () => {
     process.exit(0);
   });
 });
-// For Heroku app termination
 process.on('SIGTERM', () => {
   gracefulShutdown('Heroku app shutdown', () => {
     process.exit(0);
@@ -73,4 +59,6 @@ process.on('SIGTERM', () => {
 
 connect();
 
+// bring in the Mongoose schema
 require('./travlr');
+require('./user');
